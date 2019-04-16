2ff6dc1ad6550596b0c0930c968b26fef486d7a9
Matthew Soto <matthewsoto95@gmail.com>
2019-04-16 23:32:00 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,38 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+//var candy = "foo"
+
+function addElementToBeginningOfArray(chocolateBars, candy){
+ return [candy, ...chocolateBars];
+}
+function destructivelyAddElementToBeginningofArray(chocolateBars, candy){
+  chocolateBars.unshift(candy);
+  return chocolatebars;
+}
+function addElementToEndOfArray(chocolateBars, candy){
+  return [ ...chocolateBars, candy];
+}
+function destructivelyAddElementToEndOfArray(arr4,ele4){
+  chocolateBars.push(candy);
+  return chocolateBars;
+}
+function accessElementInArray(array,index){
+ return array[index];
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift();
+}
+function removeElementFromBeginningOfArray(array){
+  array.slice(1);
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+}
+
+
+
+
+
+
+
+
+
